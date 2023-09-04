07d8237100f47640d8184dd4a818d105e0aae374
nagarajRPoojari <np137270@gmail.com>
2023-09-05 00:36:17 +0530

    -

--- client/client/src/Components/Map.jsx
+++ client/client/src/Components/Map.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import React, { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import "mapbox-gl/dist/mapbox-gl.css";
 
